1018cc6773de1ec2ea767d61b04d076cf8b521a0
chenyk <18061446901@163.com>
2024-06-05 22:22:14 +0800

    feat: 引入Spring Cloud Gateway网关

---
+++ gulimall-gateway/src/main/java/com/cyk/gulimall/gateway/GulimallGatewayApplication.java
@@ -0,0 +1,17 @@
+package com.cyk.gulimall.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * The class GulimallProductApplication.
+ **/
+@EnableDiscoveryClient
+@SpringBootApplication
+public class GulimallGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GulimallGatewayApplication.class, args);
+    }
+}
