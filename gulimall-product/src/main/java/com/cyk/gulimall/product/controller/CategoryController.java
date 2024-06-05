ee42a4c1f9ff8ebd50b025fb27a4c5c66285c95a
chenyk <18061446901@163.com>
2024-06-05 23:40:26 +0800

    feat: 开发商品三级分类 & 配置网关路由转发

--- gulimall-product/src/main/java/com/cyk/gulimall/product/controller/CategoryController.java
+++ gulimall-product/src/main/java/com/cyk/gulimall/product/controller/CategoryController.java
@@ -1,15 +1,16 @@
 package com.cyk.gulimall.product.controller;
 
-import com.cyk.common.utils.PageUtils;
 import com.cyk.common.utils.R;
 import com.cyk.gulimall.product.entity.CategoryEntity;
 import com.cyk.gulimall.product.service.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Arrays;
-import java.util.Map;
-
+import java.util.List;
 
 
 /**
@@ -26,15 +27,14 @@ public class CategoryController {
     private CategoryService categoryService;
 
     /**
-     * 列表
+     * 查询出所有分类以及子分类，以树形结构组装起来列表
      */
-    @RequestMapping("/list")
-    public R list(@RequestParam Map<String, Object> params){
-        PageUtils page = categoryService.queryPage(params);
-
-        return R.ok().put("page", page);
+    @RequestMapping("/list/tree")
+    //@RequiresPermissions("product:category:list")
+    public R list(){
+        List<CategoryEntity> entities = categoryService.listWithTree();
+        return R.ok().put("data", entities);
     }
-
 
     /**
      * 信息
--- gulimall-product/src/main/java/com/cyk/gulimall/product/service/impl/CategoryServiceImpl.java
+++ gulimall-product/src/main/java/com/cyk/gulimall/product/service/impl/CategoryServiceImpl.java
@@ -1,16 +1,19 @@
 package com.cyk.gulimall.product.service.impl;
 
-import org.springframework.stereotype.Service;
-import java.util.Map;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.cyk.common.utils.PageUtils;
 import com.cyk.common.utils.Query;
-
 import com.cyk.gulimall.product.dao.CategoryDao;
 import com.cyk.gulimall.product.entity.CategoryEntity;
 import com.cyk.gulimall.product.service.CategoryService;
+import org.springframework.stereotype.Service;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 
 @Service("categoryService")
@@ -26,4 +29,26 @@ public class CategoryServiceImpl extends ServiceImpl<CategoryDao, CategoryEntity
         return new PageUtils(page);
     }
 
+    @Override
+    public List<CategoryEntity> listWithTree() {
+        //1、查询出所有分类
+        List<CategoryEntity> entities = super.baseMapper.selectList(null);
+
+        //2、组装成父子的树形结构
+        //2.1)、找到所有一级分类
+        return entities.stream()
+                .filter(e -> e.getParentCid() == 0)
+                .peek((menu) -> menu.setChildren(getChildren(menu, entities)))
+                .sorted(Comparator.comparingInt(menu -> (menu.getSort() == null ? 0 : menu.getSort())))
+                .collect(Collectors.toList());
+    }
+
+    //递归查找所有菜单的子菜单
+    private List<CategoryEntity> getChildren(CategoryEntity root, List<CategoryEntity> all) {
+        return all.stream().filter(categoryEntity -> categoryEntity.getParentCid().equals(root.getCatId())).peek(categoryEntity -> {
+            //1、找到子菜单(递归)
+            categoryEntity.setChildren(getChildren(categoryEntity, all));
+        }).sorted(Comparator.comparingInt(menu -> (menu.getSort() == null ? 0 : menu.getSort()))).collect(Collectors.toList());
+
+    }
 }
