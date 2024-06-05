78f14c1666143a0a2a12fb78bf11762ae57cefff
chenyk <18061446901@163.com>
2024-06-06 00:22:25 +0800

    feat: 完成商品服务三级分类展示、新增、修改、删除、批量删除

--- gulimall-product/src/main/java/com/cyk/gulimall/product/GulimallProductApplication.java
+++ gulimall-product/src/main/java/com/cyk/gulimall/product/GulimallProductApplication.java
@@ -2,10 +2,12 @@ package com.cyk.gulimall.product;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * The class GulimallProductApplication.
  **/
+@EnableDiscoveryClient
 @SpringBootApplication
 public class GulimallProductApplication {
 
