7c9b7af31c83e49bf70b84bdaba7cb89a43085ee
chenyk <18061446901@163.com>
2024-06-08 18:45:06 +0800

    feat: 商品服务-查询分组未关联的属性

--- gulimall-product/src/main/java/com/cyk/gulimall/product/controller/AttrGroupController.java
+++ gulimall-product/src/main/java/com/cyk/gulimall/product/controller/AttrGroupController.java
@@ -61,6 +61,20 @@ public class AttrGroupController {
     }
 
     /**
+     * 获取属性分组没有关联的其他属性
+     */
+    @GetMapping(value = "/{attrgroupId}/noattr/relation")
+    public R attrNoattrRelation(@RequestParam Map<String, Object> params,
+                                @PathVariable("attrgroupId") Long attrgroupId) {
+
+        // List<AttrEntity> entities = attrService.getRelationAttr(attrgroupId);
+
+        PageUtils page = attrService.getNoRelationAttr(params,attrgroupId);
+
+        return R.ok().put("page",page);
+    }
+
+    /**
      * 信息
      */
     @RequestMapping("/info/{attrGroupId}")
