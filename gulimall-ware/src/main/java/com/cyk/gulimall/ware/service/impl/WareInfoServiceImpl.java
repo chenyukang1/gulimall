48b7493995d5c25b65c6280214ec06aab8a2698b
chenyk <18061446901@163.com>
2024-06-09 20:15:26 +0800

    feat: 仓储服务-仓库管理-获取仓库列表

--- gulimall-ware/src/main/java/com/cyk/gulimall/ware/service/impl/WareInfoServiceImpl.java
+++ gulimall-ware/src/main/java/com/cyk/gulimall/ware/service/impl/WareInfoServiceImpl.java
@@ -1,16 +1,17 @@
 package com.cyk.gulimall.ware.service.impl;
 
-import org.springframework.stereotype.Service;
-import java.util.Map;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.cyk.common.utils.PageUtils;
 import com.cyk.common.utils.Query;
-
 import com.cyk.gulimall.ware.dao.WareInfoDao;
 import com.cyk.gulimall.ware.entity.WareInfoEntity;
 import com.cyk.gulimall.ware.service.WareInfoService;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.stereotype.Service;
+
+import java.util.Map;
 
 
 @Service("wareInfoService")
@@ -18,9 +19,17 @@ public class WareInfoServiceImpl extends ServiceImpl<WareInfoDao, WareInfoEntity
 
     @Override
     public PageUtils queryPage(Map<String, Object> params) {
+        QueryWrapper<WareInfoEntity> queryWrapper = new QueryWrapper<>();
+        String key = (String) params.get("key");
+        if (!StringUtils.isEmpty(key)) {
+            queryWrapper.eq("id",key)
+                    .or().like("name",key)
+                    .or().like("address",key)
+                    .or().like("areacode",key);
+        }
         IPage<WareInfoEntity> page = this.page(
                 new Query<WareInfoEntity>().getPage(params),
-                new QueryWrapper<WareInfoEntity>()
+                queryWrapper
         );
 
         return new PageUtils(page);
