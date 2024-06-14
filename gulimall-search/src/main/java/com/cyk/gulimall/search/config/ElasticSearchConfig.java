61e07c3623972620fb718fb365b87f810ff13133
chenyk <18061446901@163.com>
2024-06-14 16:25:00 +0800

    feat: 使用elasticsearch-rest-high-level-client测试API

--- gulimall-search/src/main/java/com/cyk/gulimall/search/config/ElasticSearchConfig.java
+++ gulimall-search/src/main/java/com/cyk/gulimall/search/config/ElasticSearchConfig.java
@@ -1,6 +1,7 @@
 package com.cyk.gulimall.search.config;
 
 import org.apache.http.HttpHost;
+import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestHighLevelClient;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
@@ -17,6 +18,17 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @EnableConfigurationProperties(ElasticSearchConfigProperties.class)
 public class ElasticSearchConfig {
+
+    public static final RequestOptions COMMON_OPTIONS;
+
+    static {
+        // RequestOptions.Builder builder = RequestOptions.DEFAULT.toBuilder().build() ;
+        // builder.addHeader("Authorization", "Bearer " + TOKEN);
+        // builder.setHttpAsyncResponseConsumerFactory(
+        //         new HttpAsyncResponseConsumerFactory
+        //                 .HeapBufferedResponseConsumerFactory(30 * 1024 * 1024 * 1024));
+        COMMON_OPTIONS = RequestOptions.DEFAULT.toBuilder().build();
+    }
 
     @Bean
     public RestHighLevelClient esRestClient(ElasticSearchConfigProperties properties) {
